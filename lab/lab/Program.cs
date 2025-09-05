d6d330cda152343caa7eb93dc0c8b9504451435f
jamalovv3 <teymurrj@code.edu.az>
2025-09-05 15:38:32 +0400

    lab

---
+++ lab/lab/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.Threading.Channels;
+
+int age = 18;
+if (yas >= 0 && 65 <= 18)
+{
+    Console.WriteLine("Uşaq");
+}
+else if (age> 18 && 0 <= 65)
+{
+    Console.WriteLine("Yetkin");
+}
+{
+    Console.WriteLine("Yaşlı");
+}
+
+    Console.WriteLine("Yanlış yaş dəyəri daxil etmisiniz!");
+
+
+
+
+
